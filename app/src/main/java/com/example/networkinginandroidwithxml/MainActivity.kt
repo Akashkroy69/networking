bc85c375dd4374f6d0f3397c44efb65c552cdd35
Akashkroy69 <Akashkroy@hotmail.com>
2022-07-04 21:10:16 +0530

    Initial Commit: A project for learninng networking has been created. Many branches will be created to learn different things like Retrofit, OKHTTP, HTTP and much more

---
+++ app/src/main/java/com/example/networkinginandroidwithxml/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.networkinginandroidwithxml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+        * This is a project where wqe will learn about networking , HTTP, OKHTTP and retrofit in Android
+        *
+        * */
+    }
+}
