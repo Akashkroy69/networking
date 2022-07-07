332659868cd3830009e10c68aa613e7115acbddd
Akashkroy69 <Akashkroy@hotmail.com>
2022-07-07 21:32:24 +0530

    A singleton object/class has been created named RetrofitInstance.kt This is for creating instance of the Retrofit

--- app/src/main/java/com/example/networkinginandroidwithxml/RetrofitInstance.kt
+++ app/src/main/java/com/example/networkinginandroidwithxml/RetrofitInstance.kt
@@ -3,6 +3,11 @@ package com.example.networkinginandroidwithxml
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+//This is a singleton class (also called singleton object in kotlin).
+//What is singleton class/object in kotlin?
+//Singleton Class in Kotlin is also called as the Singleton Object in Kotlin.
+// Singleton class is a class that is defined in such a way that only one instance of the class can be created and used
+// everywhere
 object RetrofitInstance {
     //What is lazy?
     // A kotlin feature for memory optimization. this creates the specific obj whenever they are needed.
